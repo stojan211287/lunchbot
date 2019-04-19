53ae623909e0540ea7b7585a92a996851fbc2f0e
Stojan Jovanovic <whyisepsilonneverlessthanzero@gmail.com>
2019-04-19 15:34:03 +0100

    Lunchbot for Cafe Magdalen now works

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import restaurant.cafe_magdalen
 
+from collections import defaultdict
+
+from argparse import ArgumentParser
+
 
 if __name__ == "__main__":
+    
+    parser = ArgumentParser()
+    parser.add_argument("--day", default="monday", dest="day")
+    args = parser.parse_args()
 
-    food_for_the_week = restaurant.cafe_magdalen.food()
-
-    for num, lst in food_for_the_week.items():
-        print(lst)
-
+    menu = restaurant.cafe_magdalen.menu()
+    
+    try:
+        print(menu[args.day.lower()])
+    except KeyError:
+        raise Exception(f"Day {args.day} does not seem to be present in the menu.")
--- restaurant/cafe_magdalen.py
+++ restaurant/cafe_magdalen.py
@@ -7,8 +7,13 @@ from io import BytesIO
 from bs4 import BeautifulSoup
 from datetime import date
 
+
 EXCLUDED = "!"
 BREAK = "__"
+JOIN_NEXT_LINE = "->"
+JOIN_PREVIOUS_LINE = "<-"
+
+DAYS_OF_WEEK = ["monday", "tuesday", "wednesday", "thursday", "friday"]
 
 
 def get_menus(food_url):
@@ -38,47 +43,73 @@ def get_menus(food_url):
     return pdf_links
 
 
-def food(food_url="http://www.oxfordsp.com/parklife/magdalen-centre/#food"):
+def parse_dishes(food_url):
 
-    pdf_links = get_menus(food_url=food_url)
+    def is_line_break(line_of_menu: str) -> bool:
+        return line_of_menu.startswith("Meat Main Course") or \
+               line_of_menu.startswith("Vegetarian") or \
+               line_of_menu.startswith("Daily Snack") or \
+               line_of_menu.startswith("Soup") or \
+               line_of_menu.startswith("Carbohydrate") or \
+               line_of_menu.startswith("Dessert")
 
     def lines_to_exclude(line_of_menu: str) -> bool:
         return line_of_menu.startswith("£") or \
                line_of_menu.startswith("(") or \
-               line_of_menu.startswith("V") or \
+               line_of_menu.startswith("V ") or \
+               line_of_menu.startswith("Vg ") or \
                line_of_menu.startswith("GF") or \
                line_of_menu.startswith("DF") or \
                line_of_menu.startswith("H ") or \
                line_of_menu.startswith("Allergen") or \
                line_of_menu == "" or \
-               line_of_menu == "H"
-
-    def is_line_break(line_of_menu: str) -> bool:
-        return line_of_menu.startswith("Meat Main Course") or \
-               line_of_menu.startswith("Vegetarian Main Course") or \
-               line_of_menu.startswith("Daily Snack") or \
-               line_of_menu.startswith("Soup") or \
-               line_of_menu.startswith("Carbohydrate") or \
-               line_of_menu.startswith("Seasonal") or \
-               line_of_menu.startswith("Dessert")
+               line_of_menu == "H" or \
+               line_of_menu == "V" or \
+               line_of_menu.startswith("Seasonal ") or \
+               line_of_menu.startswith("Vegetables 80p")
+        
+    def join_next_line(line_of_menu: str) -> bool:
+        return  line_of_menu.endswith("with") or \
+                line_of_menu.endswith("and") or \
+                line_of_menu.endswith("in") or \
+                line_of_menu.endswith("Jumbo") or \
+                line_of_menu.endswith("/")
+                
+    def join_previous_line(line_of_menu: str) -> bool:
+        return line_of_menu.startswith("topped") or \
+                line_of_menu.startswith("served") or \
+                line_of_menu.startswith("with")
+                
+    def construct_return_line(line_of_menu: str) -> str:
+        
+        return_line = line_of_menu
+        
+        if join_previous_line(return_line):
+            return_line = JOIN_PREVIOUS_LINE+" "+return_line
+        if join_next_line(return_line):
+            return_line =  return_line+" "+JOIN_NEXT_LINE
+            
+        return return_line
 
     def get_menu_item(line_of_menu: str) -> str:
-
+        
         stripped_line = line_of_menu.strip()
-
+        
         if lines_to_exclude(stripped_line):
             return EXCLUDED
         elif is_line_break(stripped_line):
             return BREAK
         else:
-            return stripped_line
+            return construct_return_line(stripped_line)
 
-    def add_clean_item_to_data_store(clean_item: str, data_list: list) -> list:
-        if not clean_item == "":
-            data_list = data_list.append(clean_item)
+    def add_item_to_data_store(item: str, data_list: list) -> list:
+        if not item == "":
+            data_list = data_list.append(item)
         return data_list
-
-    data_model = defaultdict(list)
+        
+    pdf_links = get_menus(food_url=food_url)
+        
+    dish_types = defaultdict(list)
 
     for i, (week_number, link_data) in enumerate(pdf_links.items()):
         if i == 0:
@@ -89,21 +120,52 @@ def food(food_url="http://www.oxfordsp.com/parklife/magdalen-centre/#food"):
             pdf_reader = PyPDF2.PdfFileReader(pdf_file)
 
             menu_text = pdf_reader.getPage(0).extractText().split(os.linesep)
-
-            ind = 1
+            
+            # SKIP WEEK NUMBER AND WEEKDAY NAMES
+            processed_items = [get_menu_item(menu_line) for menu_line in menu_text[7:]]
+            
+            item_index = 0
+            dish_index = 1
             parsed_menu_item = ""
+            
+            while (item_index < len(processed_items)):
+                
+                item = processed_items[item_index]
 
-            # SKIP FIRST ROW WITH NAMES OF DAYS OF THE WEEK
-            for menu_text_item in menu_text[7:]:
-
-                processed_item = get_menu_item(menu_text_item)
-
-                if processed_item == EXCLUDED:
-                    add_clean_item_to_data_store(parsed_menu_item, data_model[ind])
-                    parsed_menu_item = ""
-                elif processed_item == BREAK:
-                    ind += 1
+                if item == EXCLUDED:
+                    pass
+                elif item  == BREAK:
+                    dish_index += 1
                 else:
-                    parsed_menu_item = parsed_menu_item + " " + processed_item
+                    parsed_menu_item = item
+                    
+                    while parsed_menu_item.startswith(JOIN_PREVIOUS_LINE):
+                        cut_start = len(JOIN_PREVIOUS_LINE)+1
+                        parsed_menu_item = dish_types[dish_index][-1] + " " + parsed_menu_item[cut_start:]
+                        dish_types[dish_index].pop(-1)
+                    
+                    while parsed_menu_item.endswith(JOIN_NEXT_LINE):
+                        cut_end = len(JOIN_NEXT_LINE)+1
+                        parsed_menu_item = parsed_menu_item[:-cut_end]+ " " + processed_items[item_index+1]
+                        item_index += 1
+                        
+                add_item_to_data_store(parsed_menu_item, dish_types[dish_index])
+                parsed_menu_item = ""
+                
+                item_index += 1
 
-    return data_model
+    return dish_types
+    
+    
+def menu(food_url="http://www.oxfordsp.com/parklife/magdalen-centre/#food"):
+    
+    menu = {}
+
+    dishes = parse_dishes(food_url=food_url)
+    
+    for day_index in range(len(DAYS_OF_WEEK)):
+        day_name = DAYS_OF_WEEK[day_index]
+        
+        menu[day_name] = [dishes[dish_index][day_index] for dish_index in dishes.keys()]
+        
+    return menu
